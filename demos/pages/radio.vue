dd601836110df9ab5291bc97f8147cd6ec2b3512
Fuyuan Wang <zoeblow@gmail.com>
2018-02-05 17:12:17 +0800

    :baby_bottle: add docs

--- demos/pages/radio.vue
+++ demos/pages/radio.vue
@@ -2,12 +2,12 @@
   <div class="page">
     <fe-header slot="header" style="width:100%;position:absolute;left:0;top:0;z-index:100;">Radio</fe-header>
     <div class="fe-content">
-      <fe-group title="default">
+      <fe-group title="基本用法">
         <fe-radio :options="radio001" @on-change="change"></fe-radio>
       </fe-group>
-
-      <fe-group title="选中的 'China'">
-        <fe-radio :options="radio001" value="China"></fe-radio>
+ 
+      <fe-group title="选中的 '安徽'">
+        <fe-radio :options="radio001" value="安徽" disabled></fe-radio>
       </fe-group>
 
       <fe-group :title="'已填充选中颜色 '+radio001Value">
@@ -28,20 +28,13 @@
 export default {
   data() {
     return {
-      radio001: ["China", "Japan"],
-      radio001Value: "China",
-      radio002Value: "Japan",
+      radio001: ["安徽", "北京"],
+      radio001Value: "安徽",
+      radio002Value: "北京",
       radio003: [
-        {
-          icon: "http://temp.im/50x50/4a90e2/fff",
-          key: "001",
-          value: "radio001"
-        },
-        {
-          icon: "http://temp.im/50x50/4a90e2/fff",
-          key: "002",
-          value: "radio002"
-        }
+        { icon: "http://temp.im/50x50/f17c67/fff", key: "001", value: "珊瑚朱"},
+        { icon: "http://temp.im/50x50/008573/fff", key: "002", value: "松石绿"},
+        { icon: "http://temp.im/50x50/228fbd/fff", key: "003", value: "浅缥色"}
       ],
       selected: { color: "#4a90e2" }
     };
