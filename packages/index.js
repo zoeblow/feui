c2e04b2222e9bf84196f035aa5d3c78cb4801e8c
Fuyuan Wang <zoeblow@gmail.com>
2018-05-29 13:16:45 +0800

    :baby_bottle: 0.4.19

--- packages/index.js
+++ packages/index.js
@@ -53,7 +53,7 @@ import Toast from './toast';
 import Uploader from './uploader';
 import XAddress from './x-address';
 
-const version = '0.4.18';
+const version = '0.4.19';
 const components = [
   Actionsheet,
   Address,
